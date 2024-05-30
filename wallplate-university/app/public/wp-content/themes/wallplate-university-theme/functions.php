c78d65ac4324109e64ab0f0961e24a02b66295da
Michelle Nesbitt <mn218@nerdymail.com>
2024-05-30 08:13:53 -0400

    mn

--- wallplate-university/app/public/wp-content/themes/wallplate-university-theme/functions.php
+++ wallplate-university/app/public/wp-content/themes/wallplate-university-theme/functions.php
@@ -9,7 +9,9 @@ function university_files() {
 add_action('wp_enqueue_scripts','university_files');
 
 function university_features() {
-add_theme_support('title-tag'); 
+  register_nav_menu('headerMenuLocation','Header Menu Location');
+  add_theme_support('title-tag'); 
+
 
 
 }
--- wallplate-university/app/public/wp-content/themes/wallplate-university-theme/header.php
+++ wallplate-university/app/public/wp-content/themes/wallplate-university-theme/header.php
@@ -6,6 +6,7 @@
   <?php wp_head(); ?>  
   <title><?php echo the_title() ?></title>
 </head>
+
 <body <?php body_class(); ?>>
 <header class="site-header">
       <div class="container">
@@ -16,12 +17,14 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-            <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-              <li><a href="#">Programs</a></li>
+
+          
+           <ul>
+                   <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+        <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="#">Blog</a></li> 
             </ul>
           </nav>
           <div class="site-header__util">
